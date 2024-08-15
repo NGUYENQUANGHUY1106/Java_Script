f218461712de8fd6691128ec171d2120aff4c81d
NGUYENQUANGHUY1106 <nguyenquanghuy110605@gmail.com>
2024-08-15 22:23:24 +0700

    Lesson1_Lesson4

---
+++ lesson2/index.js
@@ -0,0 +1,4 @@
+var fullName = 'Nguyen Quang Huy';
+var age  = 26 ;
+alert(fullName); 
+alert(age);
---
+++ lesson4_built_in/main.js
@@ -0,0 +1,19 @@
+//  var fullName = 'NguyenQuangHuy'
+// console.log(fullName);
+// // in ra dòng thông báo ở console ở trình duyệt
+
+// confirm('Xac Nhan ban du tuoi');
+// xác nhận độ tuổi
+
+// prompt('Xin mời nhập tuổi của bạn');
+// hiển thị ô input để lấy dữ liệu nhập vào 
+
+// setTimeout(function() {
+//     alert('thong bao');
+// }, 1000)
+// quy định thời gian để hiển thị ra một thông báo
+
+setInterval(function()
+{
+    console.log('Thongbao' +Math.random());
+},1000)
