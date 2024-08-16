d189fd60cfc38a304ad2935bfcaae8ea2b4f18a3
NGUYENQUANGHUY1106 <nguyenquanghuy110605@gmail.com>
2024-08-16 14:28:43 +0700

    ToanTu

---
+++ lesson5_toantu/main.js
@@ -0,0 +1,29 @@
+// toán tử số học 
+//  var a = 1+2 ;
+//  console.log(a);
+
+
+// toán tử gán 
+
+//  var fullNamec = 'nguyenquanghuy';
+//  console.log(fullNamec);
+
+// toán tử so sánh 
+ 
+// var a = 1 ;
+// var b = 2 ;
+
+// if(a<b)
+// {
+//     alert('đúng');
+// }
+
+// toán tử logic
+
+// var a = 1 ;
+// var b = 2 ;
+
+// if(a > 0 && b>0)
+// { 
+//   alert ('a & b lớn hơn 0');
+// }
